8b645056ade8b3973efdd3d7cd21462e6957329a
fergie1990 <ben.ferguson90@gmail.com>
2017-03-12 01:14:39 +1000

    numbering option not complete

--- Sources/echo.swift
+++ Sources/echo.swift
@@ -1,9 +1,34 @@
 import Foundation
 
-let eof = "^Z"
-var line: String = ""
 
-while line != eof {
-  line = readLine()!
+/*while line != eof {
+  line += readLine()!
   print(line)
+}*/
+
+func getLines () -> [String] {
+
+  var i : Int = 0
+  var lines = [String] ()
+  var line : String = ""
+  let eof = "^Z"
+  lines.append("")
+  print(EOF)
+  while lines[i] != eof {
+    print(i)
+    line = readLine()!
+    lines.append(line)
+    i += 1
+  }
+  return lines
 }
+
+func printLine(input: [String]) {
+  for var i in 0..<input.count {
+    print(input[i])
+    i += 1
+  }
+}
+
+var lines = [String] (getLines())
+printLine(input: lines)
