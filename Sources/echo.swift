21102e1d3b84032e80af6ef608ac1eac56149714
fergie1990 <ben.ferguson90@gmail.com>
2017-03-13 00:11:57 +1000

    Command line args added, readstdin updated, readfile and printfile added

--- Sources/echo.swift
+++ Sources/echo.swift
@@ -1,34 +1,56 @@
 import Foundation
 
+//read from stdin complete
+func readStdin() {
 
-/*while line != eof {
-  line += readLine()!
-  print(line)
-}*/
+  var line : String? = ""
+  line = readLine()
 
-func getLines () -> [String] {
-
-  var i : Int = 0
-  var lines = [String] ()
-  var line : String = ""
-  let eof = "^Z"
-  lines.append("")
-  print(EOF)
-  while lines[i] != eof {
-    print(i)
-    line = readLine()!
-    lines.append(line)
-    i += 1
+  while line != nil {
+    print(line!)
+    line = readLine()
   }
-  return lines
 }
 
-func printLine(input: [String]) {
+//read from file
+func read (file: String) -> [String]{
+  //let fm = FileManager.default
+  //let docsurl = try! fm.url(for:.documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+  //let myurl = docsurl.appendingPathComponent(file)
+  print(file)
+  do {
+    let data = try String(contentsOfFile: file, encoding: .utf8)
+    var myStrings = [String]()
+    myStrings += (data.components(separatedBy: .newlines))
+  } catch {
+      print("Failed to read file")
+      print(error)
+  }
+return myStrings
+}
+
+//print file contents
+func output(input: [String]) {
   for var i in 0..<input.count {
     print(input[i])
     i += 1
   }
 }
 
-var lines = [String] (getLines())
-printLine(input: lines)
+for argument in CommandLine.arguments {
+    switch argument {
+    case "test1.txt":
+        print("first argument");
+        read(file: argument)
+        output(input: read(file: argument))
+
+    //case "*.txt *.txt":
+    //    print("second argument");
+
+    default:
+        readStdin()
+    }
+}
+/*
+
+*/
