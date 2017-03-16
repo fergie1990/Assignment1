d26da009a9f77c9165c307c9fec4be8a4c7424ef
fergie1990 <ben.ferguson90@gmail.com>
2017-03-16 14:08:10 +1000

    Reads command from stdin

--- Sources/cli.swift
+++ Sources/cli.swift
@@ -1,21 +1,21 @@
 import Foundation
 
-var input: String? = readLine()
-var tmp: String = ""
-
- while input != nil {
+func readStdin() -> String{
+	let input: String? = readLine()
+	var tmp: String = ""
+	
+	if input != nil {
 	 tmp = input!
 	 if tmp == "exit" {
 		 exit(EXIT_SUCCESS)
 	 }
-	 print(tmp)
-	 input = readLine()
-	 
- }
-/*
-let cmd: String = "ls"
-let path: String = "/home/ben/Assignment1/Sources/"
-let argv: [String] = [cmd, "-la", path]
+	}
+	return tmp
+}
+print("Please enter the command: \n")
+let input: String = readStdin()
+let argv = input.components(separatedBy: " ")
+let cmd = argv[0]
 var cargv = argv.map { strdup($0) }
 cargv.append(nil)
 
@@ -24,12 +24,20 @@ defer
 	for arg in cargv { free(arg) }
 	print("Memory was freed")
 }
- var pid: pid_t = pid_t()
+
+let returnVal: Int32 = execvp(cmd, cargv)
+if returnVal == -1 {
+	print("failure")
+	exit(EXIT_FAILURE)
+}
+
+print("Complete")
+/*
+var pid: pid_t = pid_t()
 let returnVal: Int32 = posix_spawnp(&pid, cmd, nil, nil, cargv, nil)
 if returnVal != 0 {
 	let error: String = String(cString: strerror(returnVal))
 	print("posix_spawnp failed with: \(error)")
 	exit(EXIT_FAILURE)
 }
-print("Complete")
 */
