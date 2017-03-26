91fc6753ad1e3bd42962ea0c6491b8a01a6c5a8b
fergie1990 <ben.ferguson90@gmail.com>
2017-03-27 08:42:43 +1000

    Implemented while loop and exit check, added readme

---
+++ Sources/README.txt
@@ -0,0 +1,5 @@
+README
+
+The problem with using execvp in this context 
+because it replaces the current process image 
+with a new process image.
--- Sources/cli.swift
+++ Sources/cli.swift
@@ -1,43 +1,76 @@
 import Foundation
-
+//implement WEXITSTATUS 
+private func WEXITSTATUS(_ status: CInt) -> CInt {
+	return (status >> 8) & 0xff
+}
+//read commands from standard input
 func readStdin() -> String{
 	let input: String? = readLine()
 	var tmp: String = ""
 	
 	if input != nil {
 	 tmp = input!
-	 if tmp == "exit" {
-		 exit(EXIT_SUCCESS)
-	 }
-	}
+ }
 	return tmp
 }
-print("Please enter the command: \n")
-let input: String = readStdin()
-let argv = input.components(separatedBy: " ")
-let cmd = argv[0]
-var cargv = argv.map { strdup($0) }
-cargv.append(nil)
-
-defer
-{
-	for arg in cargv { free(arg) }
-	print("Memory was freed")
+//Use posix_spawnp to create a copy of the current process
+func copyProcess() {
+	//declare variables needed
+	var input = String ()
+	var argv = [String] ()
+	var cmd = String ()
+	//continue to read commands until user enters "exit"
+	while cmd != "exit" {
+		print("Please enter the command: ")
+		//create array of arguments from standard input
+		input = readStdin()
+		argv = input.components(separatedBy: " ")
+		cmd = argv[0]
+		var cargv = argv.map { strdup($0) }
+		cargv.append(nil)
+		//free the memory of the array after the process is completed
+		defer
+		{
+			for arg in cargv { free(arg) }
+		}
+		//create process ID for the new process
+		var pid: pid_t = pid_t()
+		//check if posix_spawnp return failed and print the error
+		let returnVal: Int32 = posix_spawnp(&pid, cmd, nil, nil, cargv, nil)
+		if returnVal != 0{
+			let error: String = String(cString: strerror(returnVal))
+			print("posix_spwnp failed with: \(error)")
+			exit(EXIT_FAILURE)
+		}
+		//wait for the child process to end and return the error if one occurs
+		var status: Int32 = 0
+		let statusReturn = waitpid(pid, &status, 0)
+		if statusReturn != -1 {
+			print("Exit sataus: \(WEXITSTATUS(status))")
+		}
+	}
+}
+//use execvp to create a new process
+func createProcess() {
+	//get command line arguments from standard input
+	print("Please enter the command: \n")
+	let input: String = readStdin()
+	let argv = input.components(separatedBy: " ")
+	let cmd = argv[0]
+	var cargv = argv.map { strdup($0) }
+	cargv.append(nil)
+	//free memory 
+	defer
+	{
+		for arg in cargv { free(arg) }
+		print("Memory was freed")
+	}
+	//get return value and check if failed
+	let returnVal: Int32 = execvp(cmd, cargv)
+	if returnVal == -1 {
+		exit(EXIT_FAILURE)
+	}
 }
 
-let returnVal: Int32 = execvp(cmd, cargv)
-if returnVal == -1 {
-	print("failure")
-	exit(EXIT_FAILURE)
-}
-
-print("Complete")
-/*
-var pid: pid_t = pid_t()
-let returnVal: Int32 = posix_spawnp(&pid, cmd, nil, nil, cargv, nil)
-if returnVal != 0 {
-	let error: String = String(cString: strerror(returnVal))
-	print("posix_spawnp failed with: \(error)")
-	exit(EXIT_FAILURE)
-}
-*/
+copyProcess()
+//createProcess()
